238401770f2c170b825a1ab8bf572c674947db33
swor1976 <swor@ufg.br>
2026-02-11 11:17:42 -0300

    Adiciona o nome do arquivo zip
    O nome anterio era muito longo

--- code/baixa_dados.py
+++ code/baixa_dados.py
@@ -5,5 +5,6 @@ pooch.retrieve(
 	url="https://github.com/compgeolab/temperature-data/releases/download/2025-02-11/temperature-data.zip",
 	known_hash="md5:d102212049af1695b686c94ae1eea233",
 	path="data",
+	fname="temperature-data.zip",
 	processor=pooch.Unzip(extract_dir="temperature"),
 )
